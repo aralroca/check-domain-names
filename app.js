4795288aa94490fe0fbb5f8c812e8a5f02fe3b49
Aral Roca Gomez <aral-rg@hotmail.com>
2016-12-08 23:06:43 +0100

    Refactoring

--- app.js
+++ app.js
@@ -14,11 +14,11 @@ const domain = process.argv[2] || 'com'; // Default is .com
 
 const checkDomain = (name, cb) => {
     request.get(`https://uk.godaddy.com/domainsapi/v1/search/exact?q=${name}.${domain}&key=dlp_offer_com&pc=5012015&ptl=1`,
-        function (err, res) {
-            if(err) return cb(err);
+        (err, res) => {
+            if (err) return cb(err);
             const body = JSON.parse(res.body);
 
-            if(body.ExactMatchDomain.IsAvailable) {
+            if (body.ExactMatchDomain.IsAvailable) {
                 console.log('\x1b[32m', body.ExactMatchDomain.Fqdn, 'YES :D !', '\x1b[0m');
             } else {
                 console.log('\x1b[31m', body.ExactMatchDomain.Fqdn, 'NOT :(', '\x1b[0m');
